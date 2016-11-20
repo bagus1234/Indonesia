e024e9ff8d87de5d515db26771d0e705b0a7f5e7
Shafira Pramatana <firapramatana03@gmail.com>
2016-11-20 21:21:35 +0700

    Detail Senjata

--- app/src/main/java/adapter/SenjataAdapter.java
+++ app/src/main/java/adapter/SenjataAdapter.java
@@ -1,5 +1,6 @@
 package adapter;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import activity.SenjataFragment;
 import id.sch.smktelkom_mlg.project.xiirpl207172737.indonesia.R;
 import model.Senjata;
 
@@ -16,32 +18,43 @@ import model.Senjata;
  * Created by Shafira Pramatana on 11/20/2016.
  */
 public class SenjataAdapter extends RecyclerView.Adapter<SenjataAdapter.ViewHolder> {
-    ArrayList<Senjata> laguList;
+    ArrayList<Senjata> senjataList;
+    ISenjataAdapter mISenjataAdapter;
 
-    public SenjataAdapter(ArrayList<Senjata> laguList) {
-        this.laguList = laguList;
+    public SenjataAdapter(ArrayList<Senjata> senjataList) {
+        this.senjataList = senjataList;
+    }
+
+    public SenjataAdapter(SenjataFragment context, ArrayList<Senjata> senjataList) {
+        this.senjataList = senjataList;
+        mISenjataAdapter = context;
+
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_listlagu, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_listsenjata, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Senjata lagu = laguList.get(position);
-        holder.tvJudul.setText(lagu.judul);
-        holder.tvDeskripsi.setText(lagu.deskripsi);
-        holder.ivFoto.setImageDrawable(lagu.foto);
+        Senjata senjata = senjataList.get(position);
+        holder.tvJudul.setText(senjata.judul);
+        holder.tvDeskripsi.setText(senjata.deskripsi);
+        holder.ivFoto.setImageURI(Uri.parse(senjata.foto));
     }
 
     @Override
     public int getItemCount() {
-        if (laguList != null)
-            return laguList.size();
+        if (senjataList != null)
+            return senjataList.size();
         return 0;
+    }
+
+    public interface ISenjataAdapter {
+        void doClick(int pos);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -54,6 +67,14 @@ public class SenjataAdapter extends RecyclerView.Adapter<SenjataAdapter.ViewHold
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
             tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    mISenjataAdapter.doClick(getAdapterPosition());
+                }
+            });
         }
     }
 }
--- app/src/main/java/model/Senjata.java
+++ app/src/main/java/model/Senjata.java
@@ -1,18 +1,20 @@
 package model;
 
-import android.graphics.drawable.Drawable;
+import java.io.Serializable;
 
 /**
  * Created by Shafira Pramatana on 11/20/2016.
  */
-public class Senjata {
+public class Senjata implements Serializable {
     public String judul;
     public String deskripsi;
-    public Drawable foto;
+    public String detail;
+    public String foto;
 
-    public Senjata(String judul, String deskripsi, Drawable foto) {
+    public Senjata(String judul, String deskripsi, String detail, String foto) {
         this.judul = judul;
         this.deskripsi = deskripsi;
+        this.detail = detail;
         this.foto = foto;
     }
 }
